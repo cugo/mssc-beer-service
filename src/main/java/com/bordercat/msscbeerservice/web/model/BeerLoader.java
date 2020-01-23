53c1ad96615c83a70f1ebd041c05bf6ac9054c96
cugo <caglarsekmen@gmail.com>
2020-01-23 00:44:04 +0000

    JUnit test added for controller. closes #3

---
+++ src/main/java/com/bordercat/msscbeerservice/web/model/BeerLoader.java
@@ -0,0 +1,5 @@
+package com.bordercat.msscbeerservice.web.model;
+
+public enum BeerLoader {
+    BEER_1_UPC
+}
---
+++ src/main/java/com/bordercat/msscbeerservice/web/service/BeerService.java
@@ -0,0 +1,13 @@
+package com.bordercat.msscbeerservice.web.service;
+
+import com.bordercat.msscbeerservice.web.model.BeerDto;
+
+import java.util.UUID;
+
+public interface BeerService {
+    BeerDto getById(BeerDto beerDto, boolean anyBoolean);
+
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    BeerDto updateBeer(UUID beerId, BeerDto any);
+}
